441459b66ac28a85cc519552d2adb19f935bf196
Euclid System <christopherfoley@sprynet.com>
2018-04-16 11:46:13 -0400

    Commit to add wumpus to CAP6776 -working version of Wumpus Commit to add MAD6607 - Coding Theory to include files before class was dropped. Next commit will be end of semester Secret Sharing Protocols

---
+++ CAP6776 - Information and Data Retrieval/wumpus/misc/macros.h
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) 2007 Stefan Buettcher. All rights reserved.
+ * This is free software with ABSOLUTELY NO WARRANTY.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA
+ **/
+
+
+#ifndef __MISC__MACROS_H
+#define __MISC__MACROS_H
+
+
+#include <math.h>
+#include <bits/nan.h>
+
+
+// define some macros depending on the existence of some platform-specific macros (sigh...)
+
+#ifndef IsNANorINF
+	#define IsNANorINF(x) (!isnormal(x))
+#endif
+
+#ifndef isNAN
+	#ifdef isnan
+		#define isNAN(x) isnan(x)
+	#else
+		#define isNAN(x) ((IsNANorINF(x)) && (!isINF(x)))
+	#endif
+#endif
+
+#ifndef isINF
+	#ifdef isinf
+		#define isINF(x) isinf(x)
+	#else
+		#define isINF(x) ((IsNANorINF(x)) && (!isnan(x)))
+	#endif
+#endif
+
+#ifndef LROUND
+	#define LROUND(x) ((long)(x + 0.5))
+#endif
+
+#ifndef __STRING
+	#define __STRING(x) #x
+#endif
+
+#ifdef __SOLARIS__
+	// If building for Solaris, we assume that we will run on a SUN, which will
+	// give us a SIGBUS for every unaligned memory access. Thus, we will have to
+	// pad some data in the on-disk index for everything to be word-aligned.
+	#define INDEX_MUST_BE_WORD_ALIGNED 1
+#else
+	#define INDEX_MUST_BE_WORD_ALIGNED 0
+#endif
+
+
+#endif // __MISC__MACROS_H
+
+
